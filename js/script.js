392ccc2f3b190f3e6aa7231d55a0788d01768643
Isaac De La Portilla <idlportilla@gmail.com>
2023-09-10 20:29:40 -0500

    finished game. Best of 5, each round says winner and winner at the end of the game.

--- js/script.js
+++ js/script.js
@@ -1,36 +1,52 @@
+let pScore = 0
+let compScore = 0
+
 function getComputerChoice() {
   const pick = ["rock", "paper", "scissors"];
   return (randomPick = pick[Math.floor(Math.random() * pick.length)]);
 }
-//
-// function capitalizeChoice(str) {
-//   const capitalize = str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-//   return capitalize;
-// }
 
 function playerSelection() {
   return prompt("Enter rock, paper, or scissors").toLowerCase();
 }
 
-function play(playerSelection, getComputerChoice) {
-  console.log(`The computer chose ${getComputerChoice}`);
-  if (getComputerChoice === "rock" && playerSelection === "paper") {
+function play(playerSelection, computerSelection) {
+  console.log(`The computer chose ${computerSelection}`);
+  if (computerSelection === "rock" && playerSelection === "paper") {
+    pScore++
     return "Player wins!";
-  } else if (getComputerChoice === "paper" && playerSelection === "scissors") {
+  } else if (computerSelection === "paper" && playerSelection === "scissors") {
+    pScore++
     return "Player wins!";
-  } else if (getComputerChoice === "scissors" && playerSelection === "rock") {
+  } else if (computerSelection === "scissors" && playerSelection === "rock") {
+    pScore++
     return "Player wins!";
-  } else if (playerSelection === "rock" && getComputerChoice === "paper") {
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    compScore++
     return "Computer wins!";
-  } else if (playerSelection === "scissors" && getComputerChoice === "rock") {
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    compScore++
     return "Computer wins!";
-  } else if (playerSelection === "paper" && getComputerChoice === "scissors") {
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    compScore++
     return "Computer wins!";
   } else {
     return "It's a draw!";
   }
 }
 
-const computerChoice = getComputerChoice();
+const game = () => {
+  for (let i = 0; i < 5; i++) {
+    let computerChoice = getComputerChoice()
+    console.log((play(playerSelection(), computerChoice)));
+  }
+  if (pScore > compScore) {
+    return "You win!"
+  } else if (compScore > pScore){
+    return "You lost!"
+  } else {
+    return "It's a tie!"
+  }
+}
 
-console.log(play(playerSelection(), computerChoice));
+console.log(game());
